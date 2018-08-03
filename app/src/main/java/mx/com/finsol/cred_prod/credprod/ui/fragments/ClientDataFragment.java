3519186b51873550db01cc377e1b08e17a7f32b5
Emmanuel Bernal Lucero <manu2315>
2018-08-03 16:47:29 -0500

    Set 3,Listo para empezar a programar :)

--- app/src/main/java/mx/com/finsol/cred_prod/credprod/ui/fragments/ClientDataFragment.java
+++ app/src/main/java/mx/com/finsol/cred_prod/credprod/ui/fragments/ClientDataFragment.java
@@ -105,5 +105,6 @@ public class ClientDataFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+        void fragmentClienttelephone();
     }
 }
